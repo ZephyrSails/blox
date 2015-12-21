d16fc5f26fbbceac74ce3d9d4f654adc43294319
ZephyrSails <zephyrsails@gmail.com>
2015-12-21 22:37:38 +0800

    display visit count

--- app/admin/visitor.rb
+++ app/admin/visitor.rb
@@ -1,5 +1,18 @@
 ActiveAdmin.register Visitor do
 
-  permit_params :ip, :country, :state, :city, :last_visit_at, :visit_count
+  permit_params :ip, :country, :state, :city, :last_visit_at
+
+  index do
+    selectable_column
+    id_column
+    column :ip
+    column :country
+    column :state
+    column :city
+    column :last_visit_at
+    column :visit_count
+    actions
+    # column :data as "status"s
+  end
 
 end
