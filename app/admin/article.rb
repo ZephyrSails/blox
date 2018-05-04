62fd5d48c051076a9c32b4c2720a83a4ee097b1b
ZephyrSails <zephyrsails@gmail.com>
2018-05-03 22:46:56 -0700

    change date, change name to Morgan

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -1,5 +1,18 @@
 ActiveAdmin.register Article do
 
-  permit_params :title, :subtitle, :header, :body, :stamp
+  permit_params :title, :subtitle, :header, :body, :stamp, :created_at, :updated_at, :view_count
+
+  form do |f|
+    f.inputs "Article" do
+      f.input :title
+      f.input :subtitle
+      f.input :header
+      f.input :body
+      f.input :stamp
+      f.input :created_at
+      f.input :updated_at
+    end
+    f.actions
+  end
 
 end
